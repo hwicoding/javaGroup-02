28ff50766dcb0e4bef8b7d4fd31cb6ab535c320e
hwicoding <dlgnl117@gmail.com>
2023-12-04 09:22:51 +0900

    성진씨거 병합

--- Quiz_group_02/src/group_02/Quiz.java
+++ Quiz_group_02/src/group_02/Quiz.java
@@ -51,6 +51,44 @@ public class Quiz {
 		System.out.println("Sum of digits = "+sum);
 		*/
 		
+		
+		// 6번
+		/*
+		Scanner sc=new Scanner(System.in);
+		System.out.print("몇개의 숫자를 더할까요 ? : ");
+		int num=sc.nextInt();
+		System.out.println(num+"의 숫자를 입력하세요");
+		int[] arr= new int[num];
+		int sum=0;
+		
+		for(int i=0;i<arr.length;i++) {
+			int input=sc.nextInt();
+			
+			arr[i]=input;
+			sum+=input;
+		}
+		System.out.println("입력한 숫자의 합은 "+sum+" 입니다.");
+		*/
+		
+		
+		// 7번
+		/*
+		Scanner sc= new Scanner(System.in);
+		System.out.print("몇 단계의 피라미드로 구성할까 ? : ");
+		int level=sc.nextInt();
+		int[] arr= new int[level];
+		int count=1;
+		
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<=i;j++) {
+				System.out.print(String.format("%3d", count));
+				count++;
+			}
+			System.out.println();
+		*/
+		
+		
+		
 		// 8번
 		/*
 		Scanner scanner = new Scanner(System.in);
